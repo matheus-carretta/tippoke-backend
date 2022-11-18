14ed4af867447de42fd8758a2f8eb13c10072a44
Matheus <matheuscarretta@gmail.com>
2022-11-18 16:53:34 -0300

    feat: implement pokemonImage in post route

--- src/services/commentary.service.ts
+++ src/services/commentary.service.ts
@@ -1,4 +1,5 @@
 import { ICommentary, ICommentaryModel, ICommentaryService } from "../interfaces";
+import getPokemonImage from "../utils/fetchPokemonImage";
 
 export default class CommentaryService implements ICommentaryService {
   constructor(private model: ICommentaryModel) {
@@ -6,6 +7,12 @@ export default class CommentaryService implements ICommentaryService {
   }
  
   async add(commentary: ICommentary): Promise<ICommentary> {
+    const pokemonImage = await getPokemonImage(commentary.pokemonName);
+    
+    commentary.pokemonImage = !pokemonImage ?
+     "https://static.wikia.nocookie.net/bec6f033-936d-48c5-9c1e-7fb7207e28af/scale-to-width/755" :
+     pokemonImage;
+
     const newCommentary = await this.model.add(commentary);
 
     return newCommentary;
