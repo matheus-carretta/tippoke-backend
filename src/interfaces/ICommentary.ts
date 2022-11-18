8e07ed0a12631842734e4d1d6176900bc85fca3e
Matheus <matheuscarretta@gmail.com>
2022-11-18 00:33:48 -0300

    feat: create commentary interface

---
+++ src/interfaces/ICommentary.ts
@@ -0,0 +1,9 @@
+export interface ICommentary {
+  username: string,
+  email: string,
+  description: string,
+  pokemonName: string,
+  pokemonImage: string,
+  createdAt: string,
+  updatedAt?: string,
+}
