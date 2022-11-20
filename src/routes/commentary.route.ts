210d254fbb6b3ea486474f42bf00f5fe3bb64385
Matheus <matheuscarretta@gmail.com>
2022-11-19 22:37:42 -0300

    feat: add validatorCommentary in post route

--- src/routes/commentary.route.ts
+++ src/routes/commentary.route.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import CommentaryController from '../controllers/commentary.controller';
 import CommentaryService from '../services/commentary.service';
 import CommentaryModel from '../models/commentary.model';
+import validateCommentary from '../middlewares/validator.middleware';
 
 const commentaryModel = new CommentaryModel();
 const commentaryService = new CommentaryService(commentaryModel);
@@ -9,7 +10,7 @@ const commentaryController = new CommentaryController(commentaryService);
 
 const route = Router();
 
-route.post('/', (req, res) => {
+route.post('/', validateCommentary, (req, res) => {
   commentaryController.add(req, res);
 });
 
