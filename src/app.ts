5cb9f79ebade556a7608c9b0601fd8eeeca74def
Matheus <matheuscarretta@gmail.com>
2022-11-19 22:33:36 -0300

    feat: add error-middleware into the server

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import errorMiddleware from './middlewares/error.middleware';
 import { route } from './routes';
 
 const app = express();
@@ -7,6 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/commentary', route)
+app.use('/commentary', route);
+
+app.use(errorMiddleware);
 
 export default app;
