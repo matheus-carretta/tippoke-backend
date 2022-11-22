28314e56522df857b4538bf29979dcb260762940
Matheus <matheuscarretta@gmail.com>
2022-11-22 19:06:21 -0300

    feat: create commentary mocks for testing cases

---
+++ tests/commentaryMock.ts
@@ -0,0 +1,16 @@
+export const newCommentaryResponse = {
+    id: 2,
+    username: "teus",
+    email: "teus@gmail.com",
+    description: "ja enjoei",
+    pokemonName: "pikachu",
+    pokemonImage: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    createdAt: "2022-11-22T21:52:45.743Z"
+}
+
+export const newCommentaryPayload = {
+  username: 'teus',
+  email: 'teus@gmail.com',
+  description: 'ja enjoei',
+  pokemonName: 'pikachu'
+}
