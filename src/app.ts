c5cf2ccefd84b7e95a7b3f1f32f21b8feb73e8d1
Matheus <matheuscarretta@gmail.com>
2022-11-23 18:55:11 -0300

    commit: cors middleware implemented

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import errorMiddleware from './middlewares/error.middleware';
 import { route } from './routes';
 
 const app = express();
-
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  app.use(cors());
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
